3d60fb7429bd40e859e9d430419b82962523ced8
iamjumshaid <iamjumshaidkhan@gmail.com>
2024-08-24 23:33:42 +0200

    ✨ Add graph data for exercise detail

--- lib/exercise_detail_page.dart
+++ lib/exercise_detail_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+import 'package:fl_chart/fl_chart.dart';  // Import fl_chart package
 import 'partials/doctor_header.dart';
 import 'partials/doctor_footer.dart';
-import 'package:intl/intl.dart';
 
 class ExerciseDetailPage extends StatelessWidget {
   final String exerciseId;
@@ -16,6 +17,74 @@ class ExerciseDetailPage extends StatelessWidget {
   String formatDate(Timestamp timestamp) {
     DateTime date = timestamp.toDate();
     return DateFormat('yyyy-MM-dd').format(date);
+  }
+
+  List<FlSpot> _createFlSpots(Map<String, String> data) {
+    final List<FlSpot> spots = [];
+    data.forEach((key, value) {
+      double x = double.tryParse(key) ?? 0.0;
+      double y = double.tryParse(value) ?? 0.0;
+      spots.add(FlSpot(x, y));
+    });
+    return spots;
+  }
+
+  Widget _buildLineChart(List<FlSpot> spots, Color color, String xAxisLabel, String yAxisLabel) {
+    final double minY = spots.isNotEmpty ? spots.map((e) => e.y).reduce((a, b) => a < b ? a : b) - 1 : 0;
+    final double maxY = spots.isNotEmpty ? spots.map((e) => e.y).reduce((a, b) => a > b ? a : b) + 1 : 1;
+
+    return LineChart(
+      LineChartData(
+        gridData: FlGridData(show: true),
+        titlesData: FlTitlesData(
+          leftTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: (value, meta) {
+                return Text(value.toStringAsFixed(1), style: TextStyle(fontSize: 12));
+              },
+              reservedSize: 40,
+            ),
+            axisNameWidget: Text(
+              yAxisLabel,
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+            axisNameSize: 20,
+          ),
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: (value, meta) {
+                return Text(value.toInt().toString(), style: TextStyle(fontSize: 12));
+              },
+              reservedSize: 40,
+            ),
+            axisNameWidget: Text(
+              xAxisLabel,
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+            ),
+            axisNameSize: 20,
+          ),
+        ),
+        borderData: FlBorderData(
+          show: true,
+          border: Border.all(color: Colors.black, width: 1),
+        ),
+        lineBarsData: [
+          LineChartBarData(
+            spots: spots,
+            isCurved: true,
+            color: color,
+            barWidth: 3,
+            dotData: FlDotData(show: true),
+          ),
+        ],
+        minX: 0,
+        maxX: spots.isNotEmpty ? spots.last.x : 0,
+        minY: minY,
+        maxY: maxY,
+      ),
+    );
   }
 
   @override
@@ -44,93 +113,135 @@ class ExerciseDetailPage extends StatelessWidget {
             var sets = data.containsKey('sets') ? data['sets'] : 'N/A';
             var comments = data.containsKey('comments') ? data['comments'] : 'No comments';
             var emotion = data.containsKey('emotion') ? data['emotion'] : 'neutral';
+            var angleData = data['angle_data'] as Map<String, dynamic>? ?? {};
+            var temperatureData = data['temperature_data'] as Map<String, dynamic>? ?? {};
 
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Theme.of(context).primaryColor,
-                      child: Icon(Icons.fitness_center, color: Colors.white),
-                      radius: 30,
-                    ),
-                    SizedBox(width: 20),
-                    Expanded(  // This will allow the text to wrap and prevent overflow
-                      child: Text(
-                        exerciseName,
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black87,
-                        ),
-                        maxLines: 2,  // Limit to 2 lines if necessary
-                        overflow: TextOverflow.ellipsis,  // Add ellipsis if text is too long
+            return SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Theme.of(context).primaryColor,
+                        child: Icon(Icons.fitness_center, color: Colors.white),
+                        radius: 30,
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Row(
-                  children: [
-                    Icon(Icons.timer, color: Theme.of(context).primaryColor),
-                    SizedBox(width: 5),
-                    Text('Time: $timeTaken minutes'),
-                    SizedBox(width: 20),
-                    Icon(Icons.format_list_numbered, color: Theme.of(context).primaryColor),
-                    SizedBox(width: 5),
-                    Text('Sets: $sets'),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Text(
-                  'Feeling:',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).primaryColor,
+                      SizedBox(width: 20),
+                      Expanded(
+                        child: Text(
+                          exerciseName,
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87,
+                          ),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(height: 10),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.sentiment_very_dissatisfied,
-                      color: emotion == 'very_dissatisfied' ? Colors.red : Colors.grey,
-                    ),
-                    Icon(
-                      Icons.sentiment_dissatisfied,
-                      color: emotion == 'dissatisfied' ? Colors.orange : Colors.grey,
-                    ),
-                    Icon(
-                      Icons.sentiment_neutral,
-                      color: emotion == 'neutral' ? Colors.yellow : Colors.grey,
-                    ),
-                    Icon(
-                      Icons.sentiment_satisfied,
-                      color: emotion == 'satisfied' ? Colors.lightGreen : Colors.grey,
-                    ),
-                    Icon(
-                      Icons.sentiment_very_satisfied,
-                      color: emotion == 'very_satisfied' ? Colors.green : Colors.grey,
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Text(
-                  'Comments:',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).primaryColor,
+                  SizedBox(height: 20),
+                  Row(
+                    children: [
+                      Icon(Icons.timer, color: Theme.of(context).primaryColor),
+                      SizedBox(width: 5),
+                      Text('Time: $timeTaken minutes'),
+                      SizedBox(width: 20),
+                      Icon(Icons.format_list_numbered, color: Theme.of(context).primaryColor),
+                      SizedBox(width: 5),
+                      Text('Sets: $sets'),
+                    ],
                   ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  comments,
-                  style: TextStyle(fontSize: 16, color: Colors.black87),
-                ),
-              ],
+                  SizedBox(height: 20),
+                  Text(
+                    'Feeling:',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.sentiment_very_dissatisfied,
+                        color: emotion == 'very_dissatisfied' ? Colors.red : Colors.grey,
+                      ),
+                      Icon(
+                        Icons.sentiment_dissatisfied,
+                        color: emotion == 'dissatisfied' ? Colors.orange : Colors.grey,
+                      ),
+                      Icon(
+                        Icons.sentiment_neutral,
+                        color: emotion == 'neutral' ? Colors.yellow : Colors.grey,
+                      ),
+                      Icon(
+                        Icons.sentiment_satisfied,
+                        color: emotion == 'satisfied' ? Colors.lightGreen : Colors.grey,
+                      ),
+                      Icon(
+                        Icons.sentiment_very_satisfied,
+                        color: emotion == 'very_satisfied' ? Colors.green : Colors.grey,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Comments:',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    comments,
+                    style: TextStyle(fontSize: 16, color: Colors.black87),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Knee Angle:',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  Container(
+                    height: 300,  // Increased the height
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: _buildLineChart(
+                      _createFlSpots(angleData.cast<String, String>()),
+                      Colors.blue,
+                      'Time (seconds)',
+                      'Angle (degrees)',
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Temperature Graph:',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                  Container(
+                    height: 300,  // Increased the height
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: _buildLineChart(
+                      _createFlSpots(temperatureData.cast<String, String>()),
+                      Colors.red,
+                      'Time (seconds)',
+                      'Temperature (°C)',
+                    ),
+                  ),
+                ],
+              ),
             );
           },
         ),
